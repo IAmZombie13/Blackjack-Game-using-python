96ce88811f09d4d322ae30329d771cc9d2081d13
Junu <junaidasunil098@gmail.com>
2024-10-29 09:46:28 +0530

    initialised player and cards variables

---
+++ BlackJack.py
@@ -0,0 +1,108 @@
+import random as ra
+
+#Keeps Track of Cards
+cards = {
+    "Ace" : {
+        "Value" : 11,
+        "Hearts" : True,
+        "Diamonds" : True,
+        "Spades" : True,
+        "Clubs" : True       
+    },
+    "Two" : {
+        "Value" : 2,
+        "Hearts" : True,
+        "Diamonds" : True,
+        "Spades" : True,
+        "Clubs" : True       
+    },
+    "Three" : {
+        "Value" : 3,
+        "Hearts" : True,
+        "Diamonds" : True,
+        "Spades" : True,
+        "Clubs" : True       
+    },
+    "Four" : {
+        "Value" : 4,
+        "Hearts" : True,
+        "Diamonds" : True,
+        "Spades" : True,
+        "Clubs" : True       
+    },
+    "Five" : {
+        "Value" : 5,
+        "Hearts" : True,
+        "Diamonds" : True,
+        "Spades" : True,
+        "Clubs" : True       
+    },
+    "Six" : {
+        "Value" : 6,
+        "Hearts" : True,
+        "Diamonds" : True,
+        "Spades" : True,
+        "Clubs" : True       
+    },
+    "Seven" : {
+        "Value" : 7,
+        "Hearts" : True,
+        "Diamonds" : True,
+        "Spades" : True,
+        "Clubs" : True       
+    },
+    "Eight" : {
+        "Value" : 8,
+        "Hearts" : True,
+        "Diamonds" : True,
+        "Spades" : True,
+        "Clubs" : True       
+    },
+    "Nine" : {
+        "Value" : 9,
+        "Hearts" : True,
+        "Diamonds" : True,
+        "Spades" : True,
+        "Clubs" : True       
+    },
+    "Ten" : {
+        "Value" : 10,
+        "Hearts" : True,
+        "Diamonds" : True,
+        "Spades" : True,
+        "Clubs" : True       
+    },
+    "King" : {
+        "Value" : 10,
+        "Hearts" : True,
+        "Diamonds" : True,
+        "Spades" : True,
+        "Clubs" : True       
+    },
+    "Queen" : {
+        "Value" : 10,
+        "Hearts" : True,
+        "Diamonds" : True,
+        "Spades" : True,
+        "Clubs" : True       
+    },
+    "Jack" : {
+        "Value" : 10,
+        "Hearts" : True,
+        "Diamonds" : True,
+        "Spades" : True,
+        "Clubs" : True       
+    }
+}
+
+#initialising first player
+Player = [
+    {
+        "Cards" : [],
+        "Score" : 0,
+        "Lost" : False,
+        "GameSet" : False
+    }
+]
+
+
